12398f888d908ff7dfcee46e1315446851c34ad8
Pdani001 <18148949+Pdani001@users.noreply.github.com>
2026-02-08 16:41:52 +0100

    Updated publish.bat

--- publish.bat
+++ publish.bat
@@ -1,6 +1,5 @@
-dotnet publish . -p:PublishProfile=win-x64
-dotnet publish . -p:PublishProfile=linux-x64
-dotnet publish . -p:PublishProfile=osx-arm64
-butler push ./bin/Release/win-x64/ pdani/fazbear-multiplayer:win-x64 --userversion-file version.txt
-butler push ./bin/Release/linux-x64/ pdani/fazbear-multiplayer:linux-x64 --userversion-file version.txt
-butler push ./bin/Release/osx-arm64/ pdani/fazbear-multiplayer:osx-arm64 --userversion-file version.txt
+dotnet publish . -p:PublishProfile=Portable
+gamebundle -wlM --mg
+butler push ./bin/Bundled/win/ pdani/fazbear-multiplayer:win-x64 --userversion-file version.txt
+butler push ./bin/Bundled/linux/ pdani/fazbear-multiplayer:linux-x64 --userversion-file version.txt
+butler push ./bin/Bundled/mac-arm/ pdani/fazbear-multiplayer:osx-arm64 --userversion-file version.txt
